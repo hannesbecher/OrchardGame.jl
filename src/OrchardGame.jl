e3d3a8397d0011e6bff601862a5b60e16c92c86e
Hannes Becher <hannesbecher@gmx.de>
2023-08-05 23:42:55 +0100

    hints

--- src/OrchardGame.jl
+++ src/OrchardGame.jl
@@ -18,8 +18,12 @@ end
 
 # todo
 """
-Add a popat! function
-add die
-add one round functionadd rungame function
-
+@time a = map(x -> oneSim(OrchardGame.pickMax), 1:100_000)
+@time b = map(x -> oneSim(OrchardGame.pickMin), 1:100_000)
+rPosA = map(x -> x.ravenpos, a)
+rPosB = map(x -> x.ravenpos, b)
+sum(rPosA .< 6)
+sum(rPosB .< 6)
+histogram(rPosA, alpha=0.5)
+histogram!(rPosB, alpha=0.5)
 """
